8ece352b50201da881f563bdbf2f246fa5acbe68
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-03 14:41:43 -0700

    added all ingredients to js through switch case

--- src/Components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/Components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -16,6 +16,18 @@ const burgerIngredient = (props) => {
                 </div>
             )
             break;
+        case('meat'):
+            ingredient = <div className={classes.Meat}></div>;
+            break;
+        case('cheese'):
+            ingredient = <div className={classes.Cheese}></div>;
+            break;
+        case('salad'):
+            ingredient = <div className={classes.Salad}></div>;
+            break;
+        case('bacon'):
+            ingredient = <div className={classes.Bacon}></div>;
+            break;
     }
 }
 
