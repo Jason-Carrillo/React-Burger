e81a8c232f3ec3e60a854d5bb1b6c2f7ab22ba33
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-03 14:38:28 -0700

    added ingredients bread top and bottom to s

---
+++ src/Components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './BurgerIngredient.module.css'
+
+const burgerIngredient = (props) => {
+    let ingredient = null;
+
+    switch (props.type){
+        case('bread-bottom'):
+            ingredient = <div className={classes.BreadBottom}></div>;
+        break;
+        case('bread-top'):
+            ingredient = (
+                <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>;
+                    <div className={classes.Seeds2}></div>;
+                </div>
+            )
+            break;
+    }
+}
+
+export default burgerIngredient;
