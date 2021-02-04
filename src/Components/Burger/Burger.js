06ee8c2fe405ccd017774bfe5b05c8a18c57ca8e
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-04 13:27:53 -0700

    added ingredients transformed body through mapping

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -4,6 +4,14 @@ import classes from './Burger.module.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            console.log(...Array(props.ingredients[igKey]))
+            return [...Array(props.ingredients[igKey])].map((_, i) =>{
+               return <BurgerIngredient key={igKey + 1} type={igKey} />
+            });
+        });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
