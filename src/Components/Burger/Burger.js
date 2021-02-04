1f31f1846a74c1d02d0bfdb71cd96ae6b68c6923
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-04 13:48:54 -0700

    added p tag and if statement if there is no ingredients in the burger

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -4,7 +4,7 @@ import classes from './Burger.module.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) =>{
                return <BurgerIngredient key={igKey + i} type={igKey} />
             });
@@ -12,6 +12,10 @@ const burger = (props) => {
         .reduce((arr, el) => {
             return arr.concat( el );
         }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients!</p>
+    }
 
     console.log(transformedIngredients)
 
