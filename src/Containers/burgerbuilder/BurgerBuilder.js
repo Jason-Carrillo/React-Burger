1fdabd62c77c0e766bcf2e1fb35eaa8259d6e3d8
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-05 12:33:12 -0700

    added intial cost

--- src/Containers/burgerbuilder/BurgerBuilder.js
+++ src/Containers/burgerbuilder/BurgerBuilder.js
@@ -10,7 +10,8 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
 
     addIngredientHandler = (type) => {
