7e7b4ae7146c3ae76fe43f2faacb632a8509bcc1
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-05 12:42:37 -0700

    fixed typo to get the proper function needed

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -16,7 +16,7 @@ const buildControls = (props) => (
             <BuildControl
                 key={ctrl.label}
                 label={ctrl.label}
-                added={() => this.ingredientAdded(ctrl.type)}/>
+                added={() => props.ingredientAdded(ctrl.type)}/>
         ))}
     </div>
 )
