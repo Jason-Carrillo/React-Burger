6421d29893d360a15aa22afd1e402d0c7f105d9d
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-05 12:40:16 -0700

    added onclick function to more button

--- src/Components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/Components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ import classes from '../BuildControl/BuildControl.module.css'
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>More</button>
+        <button className={classes.Less} onClick={props.added}>More</button>
         <button className={classes.More}>Less</button>
     </div>
 )
