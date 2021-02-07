1ae6d63f3fc42602a91faffa7b1e0643bb6703a4
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-06 23:13:53 -0700

    added purchasable ability to button only correctly

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -24,7 +24,8 @@ const buildControls = (props) => (
 
         ))}
 
-        <button className={classes.OrderButton}>Order Now!</button>
+        <button className={classes.OrderButton}
+        disabled={props.purchasable}>Order Now!</button>
     </div>
 )
 
