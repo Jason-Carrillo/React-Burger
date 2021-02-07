6e5334ff66b17948bbf74d58eefd4f72cf9b304d
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-07 11:57:29 -0700

    added Modal of order summary to main page

--- src/Containers/burgerbuilder/BurgerBuilder.js
+++ src/Containers/burgerbuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Aux from '../../hoc/Aux'
 import Burger from "../../Components/Burger/Burger";
 import BuildControls from "../../Components/Burger/BuildControls/BuildControls";
 import Modal from '../../Components/UI/Modal/Modal'
+import OrderSummary from "../../Components/Burger/BurgerSummary/BurgerSummary";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -81,7 +82,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal />
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
