9734317f824cebb2901e9ad837acbe58acc780aa
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-08 20:21:55 -0700

    imported and passed in image

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import burgerLogo from '../../Assets/Images/burger-logo.png'
+
+const logo = (props) => (
+    <div>
+        <img src={burgerLogo} />
+    </div>
+)
+
+export default logo
