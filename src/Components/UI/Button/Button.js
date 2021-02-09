48552debe1237e6d7757c4a2ba1c06b5c07400a0
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-08 19:53:36 -0700

    added classes to button

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import classes from './Button.module.css'
 
 const button = (props) => (
     <button
+        className={[classes.Button, classes[props.buttonType]]}
         onClick={props.clicked}
     >{props.children}</button>
 )
