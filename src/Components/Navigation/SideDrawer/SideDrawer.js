77169ee45efb41fcbc991cea7cc545fa76053325
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-08 21:17:01 -0700

    removed redundant css for media queries

--- src/Components/Navigation/SideDrawer/SideDrawer.js
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -1,4 +1,5 @@
-import react from 'react';
+import React from 'react';
+
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 
@@ -8,7 +9,9 @@ const sideDrawer = (props) => {
     //...
     return (
         <div className={classes.SideDrawer}>
-            <Logo height="11%"/>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
             <nav>
                 <NavigationItems />
             </nav>
