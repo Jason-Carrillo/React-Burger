9fc312c7b3eaaac1c9dd6c8fdf184eb502dec3fb
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-08 21:36:57 -0700

    added drawer toggle

--- src/Components/Navigation/SideDrawer/SideDrawer.js
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -8,11 +8,15 @@ import Aux from '../../../hoc/Aux'
 import classes from './SideDrawer.module.css'
 
 const sideDrawer = (props) => {
-    //...
+    let attachedClasses = [classes.SideDrawer, classes.Close]
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
+
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={classes.SideDrawer}>
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
