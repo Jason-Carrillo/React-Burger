fa5696698564bc302bd2697953eb357bee5b6fd1
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-08 20:59:17 -0700

    added siderawer css and open close classes and styling

---
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
+
+const sideDrawer = (props) => {
+    //...
+    return (
+        <div>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+        </div>
+    );
+}
+
+export default sideDrawer
