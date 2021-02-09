73b4c04819781160ec58eb186c743c552acd1381
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-08 19:34:11 -0700

    added arrow function to purchasehandler

--- src/Containers/burgerbuilder/BurgerBuilder.js
+++ src/Containers/burgerbuilder/BurgerBuilder.js
@@ -72,7 +72,7 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients)
     }
 
-    purchaseHandler () {
+    purchaseHandler =  () => {
         this.setState({purchasing: true})
     }
 
