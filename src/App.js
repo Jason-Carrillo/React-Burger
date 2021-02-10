0846c02ea7db314eb668a2399e47d90b97cde0ab
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-10 14:32:01 -0700

    changed app back to prior state

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from "./Containers/burgerbuilder/BurgerBuilder";
 
 class App extends Component {
-    state = {
-        show: true
-    }
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({show: false})
-        },5000)
-    }
 
     render() {
       return (
           <div>
             <Layout>
-                {this.state.show ? <BurgerBuilder/> : null}
+                <BurgerBuilder/>
             </Layout>
           </div>
       )};
