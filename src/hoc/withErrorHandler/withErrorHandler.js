aad8f856fa9dff662fdb6e54337bdf30f78246ea
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-10 12:52:55 -0700

    added error confirmed handler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Modal from '../../Components/UI/Modal/Modal'
 import Aux from '../Aux/Aux'
 
-const withErrorHandler = (WrappedComponent) => {
-    return (props) => {
-        return (
-            <Aux>
-                <Modal>
-                    Something didn't work!
-                </Modal>
-            <WrappedComponent {...props} />
-            </Aux>
-        )
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+        state = {
+            error: null
+        }
+
+        componentDidMount() {
+            axios.interceptors.request.use(req => {
+                this.setState({error: null})
+            })
+            axios.interceptors.response.use(null, error => {
+                this.setState({error: error})
+            })
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null})
+        }
+
+        render(){
+            return (
+                <Aux>
+                    <Modal
+                        show={this.state.error}
+                        clicked={this.errorConfirmedHandler}>
+                        {this.state.error.message}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            )
+        }
     }
 }
 
