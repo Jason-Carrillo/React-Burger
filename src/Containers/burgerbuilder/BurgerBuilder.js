af14d0ef764c6ca6ca51508697a1e12e14e02c22
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-10 12:10:16 -0700

    added response to log, and error catch

--- src/Containers/burgerbuilder/BurgerBuilder.js
+++ src/Containers/burgerbuilder/BurgerBuilder.js
@@ -98,6 +98,8 @@ class BurgerBuilder extends Component {
             deliveryMethod: 'Fastest'
         }
         axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
     }
 
     render() {
