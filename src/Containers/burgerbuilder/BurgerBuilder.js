3083b06c26bb2d6e3c0c2b688795d0906a8f7c2c
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-10 12:28:51 -0700

    added spinner state changes in post function

--- src/Containers/burgerbuilder/BurgerBuilder.js
+++ src/Containers/burgerbuilder/BurgerBuilder.js
@@ -84,6 +84,8 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You got this BURGER!');
+        this.setState({loading: true})
+
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -99,8 +101,12 @@ class BurgerBuilder extends Component {
             deliveryMethod: 'Fastest'
         }
         axios.post('/orders.json', order)
-            .then(response => console.log(response))
-            .catch(error => console.log(error))
+            .then(response => {
+                this.setState({loading: false})
+            })
+            .catch(error => {
+                this.setState({loading: false})
+            })
     }
 
     render() {
