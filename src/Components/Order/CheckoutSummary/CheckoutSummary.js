4c6303d4863a1af7e128ee3d749bb3c02b49dff5
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-13 10:34:50 -0700

    added div and style burger with CS

--- src/Components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/Components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Burger from '../../Burger/Burger'
+
 const checkoutSummary = (props) => {
     return (
         <div>
             <h1>We hope it is amazing!</h1>
-            <div>
-
+            <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+                <Burger />
             </div>
         </div>
     )
