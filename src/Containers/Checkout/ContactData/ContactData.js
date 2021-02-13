b447064a83585bdc3d8504b9f7527686c3d16a31
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-13 11:31:31 -0700

    aded ful form to contactdata

--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import Button from '../../../Components/UI/Button/Button'
 
 class ContactData extends Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Enter your Contact Data</h4>
+                <form>
+                    <input type="text" name="name" placeholder="Enter Your name" />
+                    <input type="email" name="email" placeholder="Enter Your email" />
+                    <input type="text" name="street" placeholder="Enter Your Street Address" />
+                    <input type="text" name="postal" placeholder="Enter Your Postal Code" />
+                    <Button buttonType="Success" >Order</Button>
+                </form>
+            </div>
+        );
+    }
 
 }
 
