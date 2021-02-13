da46bb8f1c89705587c6d370006e814e74eff58f
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-13 10:33:46 -0700

    added checkout summary and css

---
+++ src/Components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const checkoutSummary = (props) => {
+    return (
+        <div>
+            <h1>We hope it is amazing!</h1>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default checkoutSummary()
