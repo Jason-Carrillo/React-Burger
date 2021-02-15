3b7040ad524116666e1c832c6938e85d3f355ae1
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-15 11:28:28 -0700

    addded ingredients output to the order

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import classes from './Order.module.css'
 
-const order = (props) => (
+const order = (props) => {
+    const ingredients = []
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount:props.ingredients[ingredientName]})
+    }
+
+    const ingredientsOuput = ingredients.map(ig => {
+        return <span style={{
+            textTransform: 'capitalize',
+            display: 'inline-block',
+            margin: '0 8px',
+            border: '1px solid #ccc',
+            padding: '5px'
+        }}
+            key={ig.name}>{ig.name} ({ig.amount})</span>
+    })
+
+    return (
     <div className={classes.Order}>
-        <p>Ingredients: Salad(1)</p>
+        <p>Ingredients: {ingredientsOuput}</p>
         <p>Price: <strong> USD{Number.parseFloat(props.price.toFixed(2))}</strong></p>
     </div>
-);
+    )
+};
 
 export default order;
