abfcea618a6de10d9e38576ebde453a611325c69
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-15 11:20:25 -0700

    added number total price by converting

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -4,7 +4,7 @@ import classes from './Order.module.css'
 const order = (props) => (
     <div className={classes.Order}>
         <p>Ingredients: Salad(1)</p>
-        <p>Price: <strong> USD</strong></p>
+        <p>Price: <strong> USD{Number.parseFloat(props.price.toFixed(2))}</strong></p>
     </div>
 );
 
