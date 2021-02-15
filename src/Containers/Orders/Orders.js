12371da8daa13e8a5d6947a14b40b4eab5c84f32
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-15 11:19:09 -0700

    added price and ingredients to order

--- src/Containers/Orders/Orders.js
+++ src/Containers/Orders/Orders.js
@@ -27,7 +27,9 @@ class Orders extends Component {
         return (
             <div>
                 {this.state.orders.map(order => (
-                    <Order key={order.id}/>
+                    <Order key={order.id}
+                    ingredients={order.ingredients}
+                    price={order.price}/>
                 ))}
             </div>
         )
