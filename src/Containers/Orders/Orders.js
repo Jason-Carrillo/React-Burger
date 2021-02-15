53cc03b4cb08a95e9b11d9159f41fe6b4fb04686
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-15 11:13:57 -0700

    added axios get request in orders

--- src/Containers/Orders/Orders.js
+++ src/Containers/Orders/Orders.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import axios from "../../axios-orders";
 
 import Order from '../../Components/Order/Order'
 
 class Orders extends Component {
+
+    componentDidMount() {
+        axios.get('orders.jason')
+            .then(red => {
+                this.setState({loading: false})
+            })
+            .catch(err => {
+                this.setState({loading: false})
+            })
+    }
+
     render() {
         return (
             <div>
