2e942fa690e018e9c1767be57c59c167ff06b892
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-16 13:57:54 -0700

    added valid check on delviery method

--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -84,7 +84,8 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                         ]
                 },
-                value: ''
+                value: '',
+                valid: true
             }
         },
         formIsValid: false,
