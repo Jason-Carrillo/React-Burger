138667fca04546d2cc571c2e078d4d8fdc454970
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-27 11:59:07 -0700

    added new version of state with updated ingredient

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -12,7 +12,7 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 ingredients: {
                     ...state.ingredients,
-                    [action.ingredientName]
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
                 }
             }
         case actionTypes.REMOVE_INGREDIENT:
