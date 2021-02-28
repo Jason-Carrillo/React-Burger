751bb92a02f56206c9274291d2ee34fe325be3c4
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-28 11:24:22 -0700

    moved prices to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,14 @@ const initialState = {
     totalPrice: 4
 }
 
+ 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case actionTypes.ADD_INGREDIENT:
@@ -13,7 +21,8 @@ const reducer = (state = initialState, action) => {
                 ingredients: {
                     ...state.ingredients,
                     [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                }
+                },
+                totalPrice: state.totalPrice
             }
         case actionTypes.REMOVE_INGREDIENT:
             return {
