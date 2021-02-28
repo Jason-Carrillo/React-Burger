888cd4d82cab1c52793f991a1047fd1d31598fa4
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-28 11:35:36 -0700

    added total price to mapstatetoprops function

--- src/Containers/Checkout/Checkout.js
+++ src/Containers/Checkout/Checkout.js
@@ -32,7 +32,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.ingredients,
+        price: state.totalPrice
     }
 }
 
