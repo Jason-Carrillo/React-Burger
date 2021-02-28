fe1cdbba78b25f94732510d528368c4af8a6d6ac
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-28 11:34:31 -0700

    removed will mount and imported connect

--- src/Containers/Checkout/Checkout.js
+++ src/Containers/Checkout/Checkout.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import CheckoutSummary from "../../Components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
-    state = {
-        ingredients: 
-            null,
-        price: 0,
-    }
-
-    componentWillMount() {
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {};
-        let price = 0
-        for (let param of query.entries()){
-            if(param[0] === "price") {
-                price = param[1];
-            } else {
-                ingredients[param[0]] = +param[1];
-            }
-
-        }
-        this.setState({ ingredients: ingredients, totalPrice: price })
-    }
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
