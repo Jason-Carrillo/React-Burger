679fa7374ec03228949aebd8a0d378c1d7c2f4eb
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-04 14:43:36 -0700

    changed error to props eerror

--- src/Containers/burgerbuilder/BurgerBuilder.js
+++ src/Containers/burgerbuilder/BurgerBuilder.js
@@ -18,7 +18,7 @@ class BurgerBuilder extends Component {
 
     componentDidMount() {
         console.log(this.props)
-        this.props.initIngredients();
+        this.props.initIngredients()
 
     }
 
@@ -57,7 +57,7 @@ class BurgerBuilder extends Component {
 
         let orderSummary = null;
 
-        let burger = this.state.error ? <p>Ingredients can't be loaded! </p> : <Spinner />;
+        let burger = this.props.error ? <p>Ingredients can't be loaded! </p> : <Spinner />;
 
         if (this.props.ings) {
             burger = (
