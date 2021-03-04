a246d661c62c4591e17f25a20a1970042c7fd680
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-04 15:14:29 -0700

    changed loading to props loading

--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -186,7 +186,7 @@ class ContactData extends Component {
             </form>
         );
 
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner />
         }
         return (
