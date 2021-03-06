9599c436d652af2c838a4900c82ed95f4b2265f5
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-06 10:39:48 -0700

    added reduce shorter code

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -19,21 +19,20 @@ const reducer = ( state = initialState, action) => {
     switch(action.type) {
         case actionTypes.ADD_INGREDIENT:
             const updatedIngredient = {[action.ingredientName ]: state.ingredients[action.ingredientName] + 1}
-            const updatedIngredients = updateObject(state.ingredients, updateObject)
+            const updatedIngredients = updateObject(state.ingredients, updatedIngredient)
             const updatedState = {
                 ingredients: updatedIngredients,
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
             }
             return updateObject(state, updatedState)
         case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[actionTypes.ingredientName]
-            };
+            const updatedIng = {[action.ingredientName ]: state.ingredients[action.ingredientName] - 1}
+            const updatedIngs = updateObject(state.ingredients, updatedIng)
+            const updatedSt = {
+                ingredients: updatedIngs,
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+            }
+            return updateObject(state, updatedSt)
         case actionTypes.SET_INGREDIENTS:
             return {
                 ...state,
