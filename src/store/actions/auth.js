7d16a2d15c98a363fe51ed57773540f79af7b4bb
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-08 13:47:55 -0700

    added url option to be different

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authfail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, method) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAIQ-2VgZuQoYotsPrAAZD2PQjIvl11ExY', authData)
+        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAIQ-2VgZuQoYotsPrAAZD2PQjIvl11ExY"
+        if (!isSignup) {
+            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAIQ-2VgZuQoYotsPrAAZD2PQjIvl11ExY"
+        }
+        axios.post(url, authData)
         .then(response => {
             console.log(response)
             dispatch(authSuccess(response.data))
