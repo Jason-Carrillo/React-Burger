eb4e51a7f977d401fb8bcb170aa330a7000e76a8
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-09 11:27:11 -0700

    pssed token in purchase Burger function

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -23,10 +23,10 @@ export const purchsaeBurgerStart = () => {
     }
 }
 
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = (orderData, token) => {
     return dispatch => {
         dispatch(purchsaeBurgerStart())
-        axios.post('/orders.json', orderData)
+        axios.post('/orders.json?auth=' + token, orderData)
                 .then(response => {
                     console.log(response.data);
                     dispatch(purchaseBurgerSuccess(response.data.name, orderData))
