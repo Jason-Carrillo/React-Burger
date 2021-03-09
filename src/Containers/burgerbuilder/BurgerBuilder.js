9b09a7a980500fa4dd443bdad8298b3af8e4e681
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-09 11:58:09 -0700

    added push to auth if user is not logged in

--- src/Containers/burgerbuilder/BurgerBuilder.js
+++ src/Containers/burgerbuilder/BurgerBuilder.js
@@ -35,7 +35,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler =  () => {
-        this.setState({purchasing: true})
+        if(this.props.isAuthenticated){
+            this.setState({purchasing: true})
+        } else {
+            this.props.history.push("/auth");
+        }
+       
     }
 
     purchaseCancelHandler = () => {
