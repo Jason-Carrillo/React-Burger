4cb9cfb14c4d1fa03866afdb3ea7ef0b577983a4
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-11 21:44:44 -0700

    removed this from check validity call in input changed handler

--- src/Containers/Auth/Auth.js
+++ src/Containers/Auth/Auth.js
@@ -52,7 +52,7 @@ inputChangedHandler = (event, controlName) => {
     const updatedControls = updateObject(this.state.controls, {
         [controlName]: updateObject(this.state.controls[controlName], {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+            valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
             touched: true
         })
     }) 
