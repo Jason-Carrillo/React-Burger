5bee9fe882a29d51225ea089f5795d2a7f90d01f
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-11 21:46:50 -0700

    removed check val from contact data, and the this to now be used as an import

--- src/Containers/Checkout/ContactData/ContactData.js
+++ src/Containers/Checkout/ContactData/ContactData.js
@@ -117,32 +117,13 @@ class ContactData extends Component {
 
     }
 
-    checkValidity (value, rules) {
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
 
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        return isValid;
-    }
 
     inputChangedHandler = (event, inputIdentifier) => {
         
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.validation),
+            valid: checkValidity(event.target.value, this.state.validation),
             touched: true
         }) 
         const updatedOrderForm = updateObject(this.state.orderForm, {
