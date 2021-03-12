069296ce4798440668c6f41b5bf34e361e4cb534
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-11 21:53:47 -0700

    removed function in modal that gave us unneeded log

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -10,9 +10,6 @@ class Modal extends Component {
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
-    componentWillUpdate(nextProps, nextState, nextContext) {
-        console.log("[Modal] Will Update")
-    }
 
     render() {
         return (
