fafd255d1676fb62a0cda1fc7e0166b7dd3892ad
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-12 14:58:17 -0700

    imported nav items

--- src/Components/Navigation/NavigationItems/Navigation.test.js
+++ src/Components/Navigation/NavigationItems/Navigation.test.js
@@ -1,10 +1,11 @@
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
+import NavigationItems from './NavigationItems'
 
 configure({adapter: new Adapter()})
 
 describe('<Navigationitems />', () => {
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-
+        const wrapper = shallow(<NavigationItems/>)
     });
 });
