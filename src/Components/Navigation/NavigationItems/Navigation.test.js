e0880e14ede6d9f02f55adc57c68f52067c52a14
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-13 10:53:58 -0700

    added external wrapper identification

--- src/Components/Navigation/NavigationItems/Navigation.test.js
+++ src/Components/Navigation/NavigationItems/Navigation.test.js
@@ -9,13 +9,19 @@ import NavigationItem from './NavigationItem/NavigationItem'
 configure({adapter: new Adapter()})
 
 describe('<Navigationitems />', () => {
-    it('should render two <NavigationItem /> elements if not authenticated', () => {
+    let wrapper;
+
+    beforeEach(() => {
         const wrapper = shallow(<NavigationItems/>);
+    })
+
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
+        wrapper = shallow(<NavigationItems/>);
         expect(wrapper.find(NavigationItem)).toHaveLength(2)
     });
 
     it('should render three <NavigationItem /> elements if authenticated', () => {
-        const wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper = shallow(<NavigationItems isAuthenticated />);
         expect(wrapper.find(NavigationItem)).toHaveLength(3)
     });
 });
