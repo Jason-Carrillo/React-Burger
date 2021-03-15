57af96d3e4bdbd9ec4561e166b9c385e2363bdd3
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-14 22:17:04 -0600

    added ingredient test

--- src/Containers/burgerbuilder/BurgerBuilder.test.js
+++ src/Containers/burgerbuilder/BurgerBuilder.test.js
@@ -9,5 +9,14 @@ import BuildControls from '../../Components/Burger/BuildControls/BuildControls'
 configure({adapter: new Adapter()})
 
 describe("<BurgerBuilder /", () => {
+    let wrapper;
 
+    beforeEach(() => {
+        wrapper = shallow(<BurgerBuilder />)
+    })
+
+    it("should render <BuildControls /> when receiving ingredients", () => {
+        wrapper.setProps({ings: {salad: 0}})
+        expect(wrapper.find(BuildControls)).toHaveLength(1)
+    })
 })
