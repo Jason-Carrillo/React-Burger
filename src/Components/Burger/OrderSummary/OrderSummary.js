fdde7888a81df9377ef02f59e114a9a6ca80197c
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-15 12:37:56 -0600

    changed orderSummary to function component

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button'
 
-class OrderSummary extends Component {
-    
-    render() {
+const orderSummary = props =>  {
 
         const ingredientSummary = Object.keys(this.props.ingredients)
             .map(igKey => {
@@ -30,6 +28,5 @@ class OrderSummary extends Component {
             </Aux>
         )
     }
-}
 
-export default OrderSummary;
+export default orderSummary;
