6faccc3aaf45cada9f4cd38f13b0b609ddb1c3fe
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-14 22:06:48 -0600

    added third test

--- src/Components/Navigation/NavigationItems/Navigation.test.js
+++ src/Components/Navigation/NavigationItems/Navigation.test.js
@@ -23,4 +23,8 @@ describe('<Navigationitems />', () => {
         wrapper.setProps({isAuthenticated: true})
         expect(wrapper.find(NavigationItem)).toHaveLength(3)
     });
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true)
+    });
 });
