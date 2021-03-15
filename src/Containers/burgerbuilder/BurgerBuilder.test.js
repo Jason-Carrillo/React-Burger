74cce2e19b998b2badb93aa7d102cb5a488ffd6b
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-14 22:14:27 -0600

    imported BuildControls to test file

--- src/Containers/burgerbuilder/BurgerBuilder.test.js
+++ src/Containers/burgerbuilder/BurgerBuilder.test.js
@@ -1,1 +1,13 @@
 import { BurgerBuilder } from './BurgerBuilder';
+
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls'
+
+configure({adapter: new Adapter()})
+
+describe("<BurgerBuilder /", () => {
+
+})
